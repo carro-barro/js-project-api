dc6f1fd941e08b6ec11883e91c4b65db5b3b59ba
Carolina Oldertz <carolina@MacBook-Air.local>
2026-04-27 14:06:36 +0200

    added js to authMiddleware for render debugging

--- routes/happyThoughtsRoutes.js
+++ routes/happyThoughtsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { authenticateUser } from "../middleware/authMiddleware"
+import { authenticateUser } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
