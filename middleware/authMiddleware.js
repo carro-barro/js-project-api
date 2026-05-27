1f47018c7f30c6bcd00cd9aac58279bd515bdddd
Carolina Oldertz <carolina@MacBook-Air.local>
2026-05-27 18:31:55 +0200

    debugg in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import { User } from "../models/User.js"
 
 export const authenticateUser = async (request, response, next) => {
   try {
-    const user = await User.findOne({ accessToken: request.header("Authorization").replace("Bearer", "")})
+    const user = await User.findOne({ accessToken: request.header("Authorization").replace("Bearer ", "")})
 
     if (user) {
       request.user = user
