8a840faaa8e5b3d933ad35ba7302e113eb587b06
Carolina Oldertz <carolina@MacBook-Air.local>
2026-05-27 11:49:11 +0200

    updated user model with first and last name

--- models/User.js
+++ models/User.js
@@ -2,6 +2,14 @@ import crypto from "crypto"
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
